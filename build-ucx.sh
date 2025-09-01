822c1ab49dd3bd46de29f20a7dee1ce65ce453cb
Zoltan Arnold Nagy <nag@zurich.ibm.com>
2025-09-01 18:04:52 +0200

    update ucx build script and Dockerfile to respect the version parameter

--- build-ucx.sh
+++ build-ucx.sh
@@ -2,17 +2,15 @@
 # Copyright (c) 2022-2025, NVIDIA CORPORATION.
 set -ex
 
-UCX_VERSION_TAG="v1.18.1"
 CUDA_HOME="/usr/local/cuda"
 
 git clone https://github.com/openucx/ucx.git
 
 cd ucx
-git checkout "${UCX_VERSION_TAG}"
+git checkout "$1"
 ./autogen.sh
 mkdir build-linux && cd build-linux
 ../contrib/configure-release --prefix="${CONDA_PREFIX}" --with-sysroot --enable-cma \
     --enable-mt --with-gnu-ld --with-rdmacm --with-verbs \
     --with-cuda="${CUDA_HOME}"
 make -j install
-
