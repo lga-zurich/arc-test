3470bbf05ac085ebb33115fbcda82e7be11b6d2f
Zoltan Arnold Nagy <nag@zurich.ibm.com>
2025-09-03 11:44:59 +0200

    need a v in front of the version to check out the proper tag

--- build-ucx.sh
+++ build-ucx.sh
@@ -7,7 +7,7 @@ CUDA_HOME="/usr/local/cuda"
 git clone https://github.com/openucx/ucx.git
 
 cd ucx
-git checkout "$1"
+git checkout v"$1"
 ./autogen.sh
 mkdir build-linux && cd build-linux
 ../contrib/configure-release --prefix="${CONDA_PREFIX}" --with-sysroot --enable-cma \
